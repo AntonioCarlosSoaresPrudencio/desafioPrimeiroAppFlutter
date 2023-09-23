129ec078958cca418b4c7685e58578ac6fe7de9d
Antonio Carlos Soares Prudencio <AntonioCarlosSoaresPrudencio@gmail.com>
2023-09-23 16:17:26 -0300

    :tada: Commit inicial

---
+++ android/app/src/main/kotlin/com/antonio_carlos_soares_prudencio/desafio_primeiro_app_flutter/desafio_primeiro_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.antonio_carlos_soares_prudencio.desafio_primeiro_app_flutter.desafio_primeiro_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
